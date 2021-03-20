2d1ab9150eb48f8456704213504fb914feddb069
THIRUMALA SRIKANTH HAYAGREEVA CHARYA <69465021+TSKHayagreevaC@users.noreply.github.com>
2021-03-20 17:23:13 +0530

    commit with get, post, put, delete http requests practice code by building Habit Tracker Appliction using python language.

--- main.py
+++ main.py
@@ -1,16 +1,62 @@
-# This is a sample Python script.
+import requests
+from datetime import datetime
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+USERNAME = "Srikanth"
+TOKEN = "hgjk45h3lk8sb34n2jkj3"
+GRAPH_ID = "graph001"
 
+pixela_endpoint = "https://pixe.la/v1/users"
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+user_params = {
+    "token": TOKEN,
+    "username": USERNAME,
+    "agreeTermsOfService": "yes",
+    "notMinor": "yes"
+}
 
+# response = requests.post(url=pixela_endpoint, json=user_params)
+# print(response.text)
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+graph_config = {
+    "id": GRAPH_ID,
+    "name": "Cycling Graph",
+    "unit": "Km",
+    "type": "float",
+    "color": "ajisai"
+}
+
+headers = {
+    "X-USER-TOKEN": TOKEN
+}
+
+# response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
+# print(response.text)
+
+pixela_creation_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}"
+
+# today = datetime(year=2020, month=7, day=23)
+today = datetime.now()
+# print(today.strftime("%Y%m%d"))
+
+pixel_data = {
+    "date": today.strftime("%Y%m%d"),
+    "quantity": input("How Many Kilometers Did You Cycle Today ?"),
+    }
+
+response = requests.post(url=pixela_creation_endpoint, json=pixel_data, headers=headers)
+print(response.text)
+
+update_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}/{today.strftime('%Y%m%d')}"
+
+new_pixel_data = {
+    "quantity": "4.5"
+}
+
+# response = requests.put(url=update_endpoint, json=new_pixel_data, headers=headers)
+# print(response.text)
+
+delete_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}/{today.strftime('Y%m%d')}"
+response = requests.delete(url=delete_endpoint, headers=headers)
+print(response.text)
